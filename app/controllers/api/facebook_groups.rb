d58267d981eb8d75ea9da7575e5e3adc8aa9d762
Faiz Mokhtar <mfmokhtar@gmail.com>
2016-04-14 21:10:07 +0800

    Add more FB groups.

--- app/controllers/api/facebook_groups.rb
+++ app/controllers/api/facebook_groups.rb
@@ -16,6 +16,30 @@ module Api
       {
         name: 'Programming Laman Web + UI/UX',
         id: '140411929363818'
+      },
+      {
+        name: 'UX Malaysia',
+        id: '408755619178957'
+      },
+      {
+        name: 'MYCocoaHeads',
+        id: '129320377135968'
+      },
+      {
+        name: 'WebCamp KL',
+        id: '179139768764782'
+      },
+      {
+        name: 'DevOps Malaysia',
+        id: '367080793332853'
+      },
+      {
+        name: 'Rails Girls Malaysia',
+        id: '375890172559564'
+      },
+      {
+        name: 'AWS User Group - Malaysia',
+        id: '340337879386117'
       }
     ]
   end
